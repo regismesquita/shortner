c662479911182ef38de213da52c1484e30955045
Regis David Souza Mesquita <github@rdsm.dev>
2023-10-28 20:40:06 +0100

    Better error handling, better structure, fix tests.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,83 @@
-use warp::Filter;
+use warp::{Reply};
+use warp::{Filter};
 use serde_derive::Deserialize;
 use std::collections::HashMap;
 use std::sync::{Mutex, Arc};
-use std::path::PathBuf;
 use serde_derive::Serialize;
 use std::fs::File;
 use std::io::{Read, Write};
-use serde_json::{to_string, from_str};
-
+use serde_json;
 use std::str::FromStr;
 use warp::hyper::StatusCode;
-
+use tracing::{info, error};
 
 #[derive(Deserialize)]
+#[derive(Serialize)]
 struct CreateAliasRequest { url: String }
 
+// The Db has the alias as key and url, count as values.
 type Db = Arc<Mutex<HashMap<String, (String, usize)>>>;
 
 #[tokio::main]
 async fn main() {
+    // Setup subscriber
+    tracing_subscriber::fmt::init();
+
+    // Create Database
     let db = load_db().await;
+
+    // Persist Database Every X seconds.
+    persist_db(&db);
+
+    // Routes
+    let stats = stats_route(&db);
+    let create_alias = create_alias_route(&db);
+    let view_data = view_data_route(db);
+
+    // Define routes and start server
+    let routes = stats.or(create_alias).or(view_data);
+    warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
+}
+
+fn persist_db(db: &Db) {
+// Spawn task to persist the DB to the disk periodically
     let cloned_db = db.clone();
+    info!("Spawning DB savers");
     tokio::spawn(async move {
-        let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(5));
+        let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(10));
         loop {
             interval.tick().await;
-            save_db(&cloned_db).await;
+            let save_result = save_db(&cloned_db).await;
+            if let Err(e) = save_result {
+                error!("Failed to save database: {}", e);
+            }
         }
     });
+}
 
+fn stats_route(db: &Db) -> impl Filter<Extract = impl Reply, Error = warp::Rejection> + Clone {
+// Define /stats route
     let db_stats = db.clone();
     let stats = warp::path("stats")
         .and(warp::get())
         .and(warp::any().map(move || db_stats.clone()))
         .and_then(stats);
+    stats
+}
 
+fn view_data_route(db: Db) -> impl Filter<Extract = impl Reply, Error = warp::Rejection> + Clone {
+// Define /<alias> GET route
+    let db_view_data = db.clone();
+    let view_data = warp::path::param::<String>()
+        .and(warp::get())
+        .and(warp::any().map(move || db_view_data.clone()))
+        .and_then(view_data)
+        .recover(handle_rejection);
+    view_data
+}
+
+fn create_alias_route(db: &Db) -> impl Filter<Extract = impl Reply, Error = warp::Rejection> + Clone {
+// Define /<alias> POST route
     let db_create_alias = db.clone();
     let create_alias = warp::path::param::<String>()
         .and(warp::post())
@@ -42,22 +85,7 @@ async fn main() {
         .and(warp::any().map(move || db_create_alias.clone()))
         .and_then(create_alias)
         .map(|_| warp::reply::with_status("Created", StatusCode::CREATED));
-
-    let db_view_data = db.clone();
-    let view_data = warp::path::param::<String>()
-        .and(warp::get())
-        .and(warp::any().map(move || db_view_data.clone()))
-        .and_then(view_data)
-        .recover(handle_rejection);
-
-    let routes = stats.or(create_alias).or(view_data);
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
-}
-
-#[derive(Serialize)]
-struct Stats {
-    alias: String,
-    count: usize,
+    create_alias
 }
 
 async fn load_db() -> Db {
@@ -75,15 +103,23 @@ async fn load_db() -> Db {
     }
 }
 
-async fn save_db(db: &Db) {
-    let read_db = db.lock().unwrap();
-    let serialized = serde_json::to_string(&*read_db).unwrap();
-    let mut file = File::create("db.json").expect("Unable to create file");
-    file.write_all(serialized.as_bytes()).expect("Unable to write data");
+async fn save_db(db: &Db) -> Result<(), Box<dyn std::error::Error>>  {
+    // let serialized = serde_json::to_string(&*read_db).unwrap();
+    // let mut file = File::create("db.json").expect("Unable to create file");
+    // file.write_all(serialized.as_bytes()).expect("Unable to write data");
+    let read_db = db.lock().map_err(|e| format!("Error locking the database: {}", e))?;
+    let serialized = serde_json::to_string(&*read_db)?;
+    let mut file = File::create("db.json")?;
+    file.write_all(serialized.as_bytes())?;
     drop(file);
+    Ok(())
 }
 
-
+#[derive(Serialize)]
+struct Stats {
+    alias: String,
+    count: usize,
+}
 
 async fn stats(db: Db) -> Result<impl warp::Reply, warp::Rejection> {
     let lock = db.lock().unwrap();
@@ -96,13 +132,14 @@ async fn stats(db: Db) -> Result<impl warp::Reply, warp::Rejection> {
 }
 
 async fn handle_rejection(err: warp::Rejection) -> Result<impl warp::Reply, warp::Rejection> {
+    // Log rejections
+    error!("An error occured: {:?}", &err);
     if err.is_not_found() {
         Ok(warp::reply::with_status(
             "Not Found",
             warp::http::StatusCode::NOT_FOUND,
         ))
-    }
-    else {
+    } else {
         Err(err)
     }
 }
@@ -112,6 +149,7 @@ async fn create_alias(alias: String, req: CreateAliasRequest, db: Db) -> Result<
     if lock.contains_key(&alias) {
         return Err(warp::reject::not_found());
     }
+    info!("New alias {} , to {}", alias, req.url);
     lock.insert(alias, (req.url, 0));
     Ok(())
 }
@@ -120,6 +158,9 @@ async fn view_data(alias: String, db: Db) -> Result<impl warp::Reply, warp::Reje
     let mut lock = db.lock().unwrap();
     if let Some((url, count)) = lock.get_mut(&alias) {
         *count += 1;
+        if *count % 1000 == 0 {
+            info!("{} ({}) reached {} views!", alias, url, count);
+        }
         return Ok(Box::new(warp::redirect::temporary(warp::http::Uri::from_str(url).unwrap())));
     }
     Err(warp::reject::not_found())
@@ -128,32 +169,33 @@ async fn view_data(alias: String, db: Db) -> Result<impl warp::Reply, warp::Reje
 #[cfg(test)]
 mod tests {
     use super::*;
-    use warp::test::request;
-
     #[tokio::test]
     async fn test_create_alias() {
         let db = Arc::new(Mutex::new(HashMap::new()));
         let test_request = CreateAliasRequest { url: "https://google.com".to_string() };
 
-        let resp = request::post()
+        let resp = warp::test::request()
+            .method("POST")
             .path("/example")
             .json(&test_request)
-            .reply(&create_alias_index("/example".to_string(), test_request, db))
+            .reply(&create_alias_route(&db))
             .await;
 
-        assert_eq!(resp.status(), 200);
+        assert_eq!(resp.status(), 201);
     }
 
     #[tokio::test]
     async fn test_get_data() {
+        //create a db
         let db = Arc::new(Mutex::new(HashMap::new()));
         db.lock().unwrap().insert("test".to_string(), ("https://google.com".to_string(), 0));
 
-        let resp = request::get()
+        let resp = warp::test::request()
+            .method("GET")
             .path("/test")
-            .reply(&data_index(db))
+            .reply(&view_data_route(db))
             .await;
 
-        assert_eq!(resp.status(), 302);
+        assert_eq!(resp.status(), 307);
     }
 }
